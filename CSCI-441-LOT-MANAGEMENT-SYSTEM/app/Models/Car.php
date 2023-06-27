aefd263fbfeceb3ebe110bca49486e859792bff1
Josh Puetz <jpuetz55@gmail.com>
2023-06-27 15:52:50 -0500

    Models added

--- CSCI-441-LOT-MANAGEMENT-SYSTEM/app/Models/Car.php
+++ CSCI-441-LOT-MANAGEMENT-SYSTEM/app/Models/Car.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Sales extends Model
+class Car extends Model
 {
     use HasFactory;
 }
---
+++ CSCI-441-LOT-MANAGEMENT-SYSTEM/database/migrations/2023_06_27_201024_create_sales_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id("sale_id");
+            $table->foreignIdFor(Car::class);
+            $table->foreignIdFor(User::class);
+            $table->dateTime("sale_date");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
