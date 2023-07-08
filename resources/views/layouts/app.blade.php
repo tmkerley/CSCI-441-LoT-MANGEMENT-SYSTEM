5b828d2cf7058583c8e26f11adf731179f8c86c3
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2023-07-07 23:54:59 -0500

    fix style directives

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,9 +14,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    @laravelViewsStyles
-    @livewireStyles
+    @laravelViewsStyles(laravel-views,tailwindcss,livewire)
 </head>
 <body>
     <div id="app">
@@ -78,7 +76,6 @@
             @yield('content')
         </main>
     </div>
-    @laravelViewsStyles
-    @livewireScripts
+    @laravelViewsScripts(laravel-views,livewire,alpine)
 </body>
 </html>
