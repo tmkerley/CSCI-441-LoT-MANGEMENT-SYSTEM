6356b8fb8d731f9a32913518e0d7ec1ac47d3a2d
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2023-07-12 19:14:23 -0500

    add project title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>CSCI-441-LOT-MANGEMENT-SYSTEM
+</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
