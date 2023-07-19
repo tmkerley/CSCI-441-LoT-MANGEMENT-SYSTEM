f8be5cfcc8a26e46c0f6fb48d60fee6a0447a467
Josh Puetz <jpuetz55@gmail.com>
2023-07-19 13:54:52 -0500

    routes for new controllers

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SpaceController;
 use App\Http\Controllers\CarController;
+use App\Http\Controllers\UserController;
 use Yajra\DataTables\Facades\DataTables;
 
 /*
@@ -25,15 +26,40 @@ Auth::routes();
 Route::get('home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::get('cars', [CarController::class, 'index'])->name('cars.index');
+Route::get('cars', [CarController::class, 'index'])->name('cars.index')->middleware('auth');
 
-Route::get('/map/{id}/', function ($id) {
+Route::get('map/{id}/', function ($id) {
     return view('map', ['space' => \App\Models\Space::find($id)]);
-})->name('map');
+})->middleware('auth')->name('map');
 
-Route::post('spaces/updateSpace/{id}',[SpaceController::class, 'updateSpace']);
+//Cars
+//Route::get('/cars', 'CarController@index')->name('cars.index');
+//Route::post('/cars', 'CarController@create')->name('cars.create');
+//Route::get('/cars/{id}', 'CarController@show')->name('cars.show');
+//Route::put('/cars/{id}', 'CarController@update')->name('cars.update');
+//Route::delete('/cars/{id}', 'CarController@destroy')->name('cars.destroy');
 
-Route::post('cars/registerMove/{id}',[CarController::class, 'registerMove']);
+//Spaces
+Route::get('spaces', [SpaceController::class, 'index'])->name('spaces.index');
+Route::post('spaces', [SpaceController::class, 'create'])->name('spaces.create');
+Route::get('spaces/{id}', [SpaceController::class, 'show'])->name('spaces.show');
+Route::put('spaces/{id}', [SpaceController::class, 'update'])->name('spaces.update');
+Route::delete('spaces/{id}', [SpaceController::class, 'destroy'])->name('spaces.destroy');
 
-Route::get('cars/registerPark/{id}', [CarController::class, 'registerPark']);
-Route::post('cars/registerPark/{id}', [CarController::class, 'confirmPark']);
+Route::post('cars/registerMove/{id}',[CarController::class, 'registerMove'])->middleware('auth');
+
+Route::get('cars/registerPark/{id}', [CarController::class, 'registerPark'])->middleware('auth');
+Route::post('cars/registerPark/{id}', [CarController::class, 'confirmPark'])->middleware('auth');
+
+//Users
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::post('/users', [UserController::class, 'create'])->name('users.create');
+Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+
+
+
+
+
