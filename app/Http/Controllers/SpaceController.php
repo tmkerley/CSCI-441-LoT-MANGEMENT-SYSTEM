3b6592f1078e1a7732cb4822ed263b78f9cec80f
Josh Puetz <jpuetz55@gmail.com>
2023-07-19 13:56:16 -0500

    add crud to spacecontroller

--- app/Http/Controllers/SpaceController.php
+++ app/Http/Controllers/SpaceController.php
@@ -5,28 +5,75 @@ namespace App\Http\Controllers;
 
 use App\Models\Space;
 use App\Models\Car;
+use Yajra\DataTables\DataTables;
+use App\DataTables\SpacesDataTable;
 use Illuminate\Http\Request;
 
 class SpaceController extends Controller
 {
-   
-    public function updateSpace(Request $request,$id) {
-      $newSpaceId = $request->input('emptySpaces');
-      $carId = $request->input('car');
-      $car = Car::find($carId);
-      $newSpace = Space::find($newSpaceId);
-      $oldSpace= Space::find($car->space_id);
-      $oldSpace->car_vinNo = NULL;
-      $oldSpace->status = 0;                   //change old space to empty (NULL)
-      $car->space_id = $newSpaceId;            //update car to new space
-      $car->isBeingMoved = 0;                  //unflag car as being moved
-      
-      $newSpace->car_vinNo = $car->vinNo; 
-      $newSpace->status = 1;                  //assign space to new car
 
-      $car->save();
-      $newSpace->save();
-      $oldSpace->save();
-      return redirect("/cars");
+
+   public function index(SpacesDataTable $dataTable)
+    {
+        return $dataTable->render('spaces.index');
+    }
+
+   public function create(Request $request)
+   {
+    $data = $request->validate([
+        'car_id' => 'nullable|exists:cars,id', // Make sure the car_id exists in the cars table
+        'status' => 'required|boolean',
+        'latitude' => 'required|numeric',
+        'longitude' => 'required|numeric',
+    ]);
+
+    $space = Space::create($data);
+
+    return redirect()->route('spaces.index')->with('success', 'Space created successfully!');
    }
+
+   public function show($id)
+   {
+    $space = Space::find($id);
+
+    if (!$space) {
+        return redirect()->route('spaces.index')->with('error', 'Space not found!');
+    }
+
+    return view('spaces.show', ['space' => $space]);
+   }
+
+   public function update(Request $request, $id)
+   {
+    $space = Space::find($id);
+
+    if (!$space) {
+        return redirect()->route('spaces.index')->with('error', 'Space not found!');
+    }
+
+    $data = $request->validate([
+        'car_id' => 'nullable|exists:cars,id', // Make sure the car_id exists in the cars table
+        'status' => 'required|boolean',
+        'latitude' => 'required|numeric',
+        'longitude' => 'required|numeric',
+    ]);
+
+    $space->update($data);
+
+    return redirect()->route('spaces.index')->with('success', 'Space updated successfully!');
+   }
+
+   public function destroy($id)
+   {
+    $space = Space::find($id);
+
+    if (!$space) {
+        return redirect()->route('spaces.index')->with('error', 'Space not found!');
+    }
+
+    $space->delete();
+
+    return redirect()->route('spaces.index')->with('success', 'Space deleted successfully!');
+   }
+
 }
