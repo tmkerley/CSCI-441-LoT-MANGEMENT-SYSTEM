efdf289e77ab381b1d01cc04ec19167170b08b8c
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2023-07-19 18:30:32 -0500

    admin controller functions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,24 +5,24 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\DataTables\adminCarsDataTable;
-use App\DataTables\adminSpacesDataTable;
-use App\DataTables\adminUsersDataTable;
+use App\DataTables\SpacesDataTable;
+use App\DataTables\UsersDataTable;
 use Yajra\DataTables\DataTables;
 
 class AdminController extends Controller
 {
     //
-    public function index(adminCarsDataTable $dataTable)
+    public function getCars(adminCarsDataTable $dataTable)
     {
         return $dataTable->render('admin.cars');
     }
 
-    public function getSpaces(adminSpacesDataTable $dataTable)
+    public function getSpaces(SpacesDataTable $dataTable)
     {
         return $dataTable->render('admin.spaces');
     }
 
-    public function getUsers(adminUsersDataTable $dataTable)
+    public function getUsers(UsersDataTable $dataTable)
     {
         return $dataTable->render('admin.users');
     }
