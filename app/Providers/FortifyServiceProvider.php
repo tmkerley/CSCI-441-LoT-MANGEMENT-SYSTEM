a08d18d05e900f178bb862ececf71f6ccd03a8ea
Josh Puetz <jpuetz55@gmail.com>
2023-07-19 13:55:49 -0500

    add fortify

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -28,6 +28,11 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        
+        Fortify::loginView(function () {
+        return view('auth.login');
+        });
+        
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
