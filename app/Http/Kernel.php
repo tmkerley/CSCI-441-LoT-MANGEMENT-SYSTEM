a53be00881fe5b809b76d9aa62049ca5b6069e5c
Joshua Puetz <45668508+Jpuetz55@users.noreply.github.com>
2023-07-19 19:11:52 -0500

    add alias to kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,7 +54,8 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        'isAdmin' => \App\Http\Middleware\Role::class,
+        'isAdmin' => \App\Http\Middleware\CheckRole::class,
+        'RDfromRoot' => \App\Http\Middleware\RedirectBasedOnRole::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
